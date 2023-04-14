28f27e999327c396c86a4fd7128ac08bc80aa06f
jzenguw <justinzeng007@gmail.com>
2023-04-14 14:13:23 -0700

    stash 2

--- screens/PantryScreen.js
+++ screens/PantryScreen.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
-import { Card, ListItem } from 'react-native-elements'
+import { ScrollView, StyleSheet, Button, Text, View, Image } from 'react-native';
 import { pantryContent } from '../data/products.js';
 
 export default function PantryScreen() {
   const [pantry, setPantry] = useState(pantryContent);
-  
+
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView>
       <Text>Pantry Screen</Text>
       <View style={styles.container}>
         <Text style={styles.title}>Pantry</Text>
           {pantry.map(item => (
-            <Card>
-              <Card.Title style={styles.paragraph}>{item.name}</Card.Title>
-              <Card.Divider/>
-              <Card.Image source={item.img} />
-              <Button icon={<Icon name='code' color='#ffffff' />} buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}} title='VIEW NOW' />
-            </Card>
+            // <Card>
+            //   <Card.Title style={styles.paragraph}>{item.name}</Card.Title>
+            //   <Card.Divider/>
+            //   <Card.Image source={"../" + item.img} />
+            //   <Button icon={<Icon name='code' color='#ffffff' />} buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}} title='VIEW NOW' />
+            // </Card>
+            <View key={item.name} style={styles.box}>
+              <Image style={styles.image} source={require('../product_img/apple.jpg')} />
+              <Text style={styles.name}>{item.name}</Text>
+            </View>
           ))}
       </View>
     </ScrollView>
@@ -26,8 +29,32 @@ export default function PantryScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    width: '45%',
+    height: 200,
+    margin: 5,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: 120,
+  },
+  title: {
+    padding: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  name: {
+    padding: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+    backgroundColor: '#97BE11',
+    color: 'white',
   },
 });
--- screens/WasteScreen.js
+++ screens/WasteScreen.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function WasteScreen() {
+  
   return (
     <View style={styles.container}>
-      <Text>Waste Screen</Text>
+      <Text style={styles.title}>Monthly Food Waste Report</Text>
+      <View style={styles.box}>
+        <Text>D3 or react native svg chart library</Text>
+      </View>
+      <Text style={styles.subtitle}>Resources</Text>
+      <View style={styles.box}>
+
+      </View>
     </View>
   );
 }
@@ -15,4 +23,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    width: '45%',
+    height: 200,
+    margin: 5,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  title: {
+    padding: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  subtitle: {
+    padding: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 });
