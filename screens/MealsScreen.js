de38715ce63a7eff86aae7bec194d35b532c76b0
Dhruv Karia <dhruvkaria@hotmail.com>
2023-04-28 14:15:00 -0700

    done

--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -2,47 +2,45 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import firebase from '../Firebase';
-import RecipeCard from './RecipeCard';
 
 const MealsScreen = () => {
   const [recipes, setRecipes] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const user = firebase.auth().currentUser;
+      const preferencesRef = firebase.database().ref(`users/${user.uid}/preferences`);
+
+      preferencesRef.once('value', (snapshot) => {
+        const preferencesData = snapshot.val();
+        if (preferencesData) {
+          const { cuisines, diet } = preferencesData;
+          const cuisineS = cuisines[0];
+          const dietS = diet;
+
+          axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=61409bc29a88436882fe11bf76ad7155&diet=${dietS}&cuisine=${cuisineS}`)
+            .then(response => {
+              setRecipes(response.data.results);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }
+      });
+    };
+
+    fetchRecipes();
+  }, []);
 
   const searchRecipes = () => {
-    axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&number=10&apiKey=54f02b231f7d4dd48e5573927c3d0edd`)
+    axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&number=10&apiKey=61409bc29a88436882fe11bf76ad7155`)
       .then(response => {
         setRecipes(response.data.results);
       })
       .catch(error => {
         console.log(error);
       });
-  };
-
-  const user = firebase.auth().currentUser;
-  const preferencesRef = firebase.database().ref(`users/${user.uid}/preferences`);
-
-  preferencesRef.once('value', (snapshot) => {
-    const preferencesData = snapshot.val();
-    const { cuisines, diet } = preferencesData;
-    const cuisineS = cuisines[0];
-    const dietS = diet;
-
-    axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=54f02b231f7d4dd48e5573927c3d0edd&diet=${dietS}&cuisine=${cuisineS}`)
-      .then(response => {
-        setRecipes(response.data.results);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  });
-
-  const handleRecipeSelect = (recipe) => {
-    setSelectedRecipe(recipe);
-  };
-
-  const handleRecipeDeselect = () => {
-    setSelectedRecipe(null);
   };
 
   return (
@@ -58,17 +56,14 @@ const MealsScreen = () => {
           <Text style={styles.searchButtonText}>Search</Text>
         </TouchableOpacity>
       </View>
-      {selectedRecipe ?
-        <RecipeCard recipe={selectedRecipe} onDeselect={handleRecipeDeselect} /> :
-        <View style={styles.container}>
-          {recipes.map(recipe => (
-            <TouchableOpacity key={recipe.id} style={styles.box} onPress={() => handleRecipeSelect(recipe)}>
-              <Image style={styles.image} source={{ uri: recipe.image }} />
-              <Text style={styles.title}>{recipe.title}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
-      }
+      <View style={styles.container}>
+        {recipes.map(recipe => (
+          <View key={recipe.id} style={styles.box}>
+            <Image style={styles.image} source={{ uri: recipe.image }} />
+            <Text style={styles.title}>{recipe.title}</Text>
+          </View>
+        ))}
+      </View>
     </ScrollView>
   );
 };
@@ -122,45 +117,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
   },
-  cardContainer: {
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    overflow: 'hidden',
-    margin: 10,
-    padding: 10,
-  },
-  cardImage: {
-    width: '100%',
-    height: 200,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  cardTitle: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  cardSubtitle: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  cardText: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  cardButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    backgroundColor: '#009688',
-    borderRadius: 10,
-    alignSelf: 'center',
-    marginTop: 20,
-  },
-  cardButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
 });
+
 
 export default MealsScreen;
